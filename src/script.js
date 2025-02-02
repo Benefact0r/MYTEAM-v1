65d653d4e8f5234063f5ebecf71b803007b54945
MASUR89 <surmava9@gmail.com>
2025-02-02 20:22:04 +0400

    added button rotation

--- src/script.js
+++ src/script.js
@@ -10,4 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".contact").addEventListener("click", function () {
         window.location.href = "contact.html";
     });
+    
+    document.querySelectorAll(".button").forEach(button => {
+        button.addEventListener("click", function () {
+            let svg = button.querySelector("svg");
+
+            if (button.classList.contains("pink")) {
+                button.classList.remove("pink");
+                button.classList.add("blue");
+                if (svg) svg.style.visibility = "hidden"; 
+            } else {
+                button.classList.remove("blue");
+                button.classList.add("pink");
+                if (svg) svg.style.visibility = "visible"; 
+            }
+        });
+    });
 });
