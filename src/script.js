a5c39a9ea7cdb6ce606d3d83a1dbaf0d2ebe099e
Benefact0r <lukagvenetadze04@gmail.com>
2025-02-04 19:07:46 +0400

    compiled script.js

--- src/script.js
+++ src/script.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".about").addEventListener("click", function () {
         window.location.href = "about.html";
     });
-    
+
     document.querySelector(".home").addEventListener("click", function () {
-        window.location.href = "indexHome.html";
+        window.location.href = "index.html";
     });
-    
+
     document.querySelector(".top-right").addEventListener("click", function () {
         window.location.href = "contact.html";
     });
-    
+
     document.querySelectorAll(".button").forEach(button => {
         button.addEventListener("click", function () {
             let svg = button.querySelector("svg");
@@ -19,14 +19,14 @@ document.addEventListener("DOMContentLoaded", function () {
             if (button.classList.contains("pink")) {
                 button.classList.remove("pink");
                 button.classList.add("blue");
-                if (svg) svg.style.visibility = "hidden"; 
+                if (svg) svg.style.visibility = "hidden";
             } else {
                 button.classList.remove("blue");
                 button.classList.add("pink");
-                if (svg) svg.style.visibility = "visible"; 
+                if (svg) svg.style.visibility = "visible";
             }
             if (targetSlide) {
-                targetSlide.classList.toggle('show'); 
+                targetSlide.classList.toggle('show');
             }
         });
     });
