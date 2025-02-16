2dab4e2329ecede2aa8cc0a74780cec180b99d23
MASUR89 <surmava9@gmail.com>
2025-02-16 11:38:53 +0400

    Local storage code

--- script.js
+++ script.js
@@ -39,8 +39,40 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+
+    // --------------------------------------------------------------local storage
+    const form = document.querySelector(".registration-form");
+    const submitBtn = document.getElementById("submit");
+
+    const savedData = JSON.parse(localStorage.getItem("contactData"));
+    if (savedData) {
+        document.getElementById("name").value = savedData.name || "";
+        document.getElementById("email").value = savedData.email || "";
+        document.getElementById("comp-name").value = savedData.company || "";
+        document.getElementById("title").value = savedData.title || "";
+        document.getElementById("message").value = savedData.message || "";
+    }
+
+    submitBtn.addEventListener("click", function (event) {
+        event.preventDefault(); 
+
+        const formData = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            company: document.getElementById("comp-name").value,
+            title: document.getElementById("title").value,
+            message: document.getElementById("message").value
+        };
+
+        localStorage.setItem("contactData", JSON.stringify(formData)); 
+
+        form.reset();
+
+        alert("Your message has been saved!"); 
+    });
 });
 
+// ---------------------------------------------------burger-menu
 const burMenu = this.document.querySelector(".menu");
 const mobMenu = document.querySelector(".mobile-menu");
 const shadow = document.querySelector(".shadow");
@@ -48,5 +80,7 @@ const shadow = document.querySelector(".shadow");
 burMenu.addEventListener("click", () => {
     burMenu.classList.toggle("active");
     mobMenu.classList.toggle("active");
-    shadow.classList.toggle("active"); // ჩნდება ან ქრება ჩაბნელება
+    shadow.classList.toggle("active"); 
 });
+
+
